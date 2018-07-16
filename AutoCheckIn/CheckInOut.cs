c9970d30a44cd88638ce51997b778a35e862d3ef
Luke <n696395@hotmail.com>
2018-07-16 17:40:48 +0800

    nitial!:!:

---
+++ AutoCheckIn/CheckInOut.cs
@@ -0,0 +1,211 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoCheckIn
+{
+    public class CheckInOut
+    {
+        private IWebDriver _Driver;
+        private string _URL = "";
+        private string _Name = "";
+        private checkstatus _Status;
+        private TimeSpan _timeout = TimeSpan.FromSeconds(10);
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CheckInOut()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="WebDriver">Web Driver</param>
+        /// <param name="URL">Web address</param>
+        /// <param name="Name">your name</param>
+        /// <param name="status">In or out</param>
+        public CheckInOut(IWebDriver WebDriver, string URL,string Name, checkstatus status)
+        {
+            _Driver = WebDriver;
+            _URL = URL;
+            _Name = Name;
+            _Status = status;
+        }
+
+        /// <summary>
+        /// Start Check In/Out
+        /// </summary>
+        public void startcheckInOut()
+        {
+            try
+            {
+                _Driver.Navigate().GoToUrl(_URL);
+                WebDriverWait wait = new WebDriverWait(_Driver, _timeout); //Wait page load complete
+
+                IWebElement left_displayname = _Driver.FindElement(By.Name("left_displayname"));
+                left_displayname.SendKeys(_Name);
+
+                IWebElement left_inout = _Driver.FindElement(By.Name("left_inout"));
+                if(_Status == checkstatus.In)
+                    left_inout.SendKeys("上班");
+                else //checkstatus.Out
+                    left_inout.SendKeys("下班");
+
+                IWebElement submit_button = _Driver.FindElement(By.Name("submit_button"));
+                //submit_button.Click();
+            }
+            catch(WebDriverTimeoutException toex)
+            {
+                Console.WriteLine("[TimeOut]"+toex.ToString());
+                throw toex;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Capture current web view
+        /// </summary>
+        public void TakingScreenShoot(string timing)
+        {
+            if(_Driver == null) return;
+            Screenshot ss = ((ITakesScreenshot)_Driver)?.GetScreenshot();
+            ss.SaveAsFile($"{Directory.GetCurrentDirectory()}\\ScreenShoot\\{DateTime.Now.ToString("yyyyMMddHHmmss")+ timing}.png", ScreenshotImageFormat.Png);
+        }
+
+        /// <summary>
+        /// Start Check In/Out
+        /// </summary>
+        /// <param name="status"> Check Status</param>
+        public void startcheck(checkstatus status)
+        {
+            _Status = status;
+            startcheckInOut();
+        }
+
+        /// <summary>
+        /// Check if already check In/Out
+        /// </summary>
+        /// <param name="status">In/Out</param>
+        /// <returns>true mean already check in/out, false mean not.</returns>
+        public bool IsChecked(string Name, checkstatus status)
+        {
+            try
+            {
+                _Driver.Navigate().GoToUrl(_URL);
+                WebDriverWait wait = new WebDriverWait(_Driver, _timeout); //Wait page load complete
+
+                IWebElement table = _Driver.FindElement(By.ClassName("right_main")).FindElement((By.ClassName("right_main_text")));
+                IReadOnlyList<IWebElement> tableRow = table.FindElements(By.TagName("tr"));
+
+                string ChtStatus = status == checkstatus.In ? "上班" : "下班";
+
+                var ss = from row in tableRow where row.Text.Contains(Name) && row.Text.Contains(ChtStatus) select row;
+                if (ss.Count() > 0)
+                {
+                    foreach (var row in tableRow)
+                    {
+                        if (row.Text.Contains(Name) && row.Text.Contains(ChtStatus))
+                        {
+                            Console.WriteLine($"Check In/Out OK, find=>[{row.Text}]");
+                            return true;
+                        }
+                    }
+                }
+                Console.WriteLine($"Check In/Out Fail, Can not find Name={Name}, CheckIn/Out={status}");
+                return false;
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Close the Web Driver
+        /// </summary>
+        public void close()
+        {
+            _Driver?.Close();
+            _Driver?.Dispose();
+        }
+
+        /// <summary>
+        /// URL string
+        /// </summary>
+        public string URL
+        {
+            get
+            {
+                return _URL;
+            }
+            set
+            {
+                _URL = value;
+            }
+        }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
+        /// <summary>
+        /// Check In or Out
+        /// </summary>
+        public checkstatus CheckStatus
+        {
+            get
+            {
+                return _Status;
+            }
+            set
+            {
+                _Status = value;
+            }
+        }
+
+        /// <summary>
+        /// Page load time out, default is 10 sec.
+        /// </summary>
+        public TimeSpan Timeout
+        {
+            get
+            {
+                return _timeout;
+            }
+            set
+            {
+                _timeout = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check Status
+    /// </summary>
+    public enum checkstatus:byte
+    {
+        In=1,
+        Out=2
+    }
+}
---
+++ AutoCheckIn/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System.Threading;
+using System.Xml;
+
+namespace AutoCheckIn
+{
+    class Program
+    {
+        static string configFileNamePath = "./config.xml";
+        static Random rnd = new Random();
+        static int DelaySecond = rnd.Next(1, 900); //1sec to 900 sec(15 min)
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //Read config
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.Load(configFileNamePath);
+                string Name = xmldoc.SelectSingleNode("/root/name").InnerText.Trim();
+                string URL = xmldoc.SelectSingleNode("/root/url").InnerText.Trim();
+
+                //create Driver
+                IWebDriver driver = new PhantomJSDriver();
+                checkstatus status = (checkstatus)Enum.Parse(typeof(checkstatus), args[0]);
+               CheckInOut chk = new CheckInOut(driver, URL, Name, status);
+
+                //Check before chcek In/Out
+                bool before_result = chk.IsChecked(Name, status);
+                if(before_result == true)//Already Check
+                {
+                    chk.TakingScreenShoot("BeforeCheckInOut");
+                    Console.WriteLine($"Already checked, Name={Name}, Status={args[0]}, Time={DateTime.Now.ToString("yyyyMMddHHmmss")}");
+                    chk?.close();
+                    return;
+                }
+
+                //delay
+                Thread.Sleep(DelaySecond * 1000);
+
+                //start check
+                chk.startcheckInOut();
+
+                bool after_result = chk.IsChecked(Name, status);
+                chk.TakingScreenShoot("AfterCheckInOut");
+                if (after_result == false)
+                    Console.WriteLine($"Check Failed, Name={Name}, Status={args[1]}, Time={DateTime.Now.ToString("yyyyMMddHHmmss")}");
+                else
+                    Console.WriteLine($"Check OK, Name={Name}, Status={args[1]}, Time={DateTime.Now.ToString("yyyyMMddHHmmss")}");
+
+                chk.close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
